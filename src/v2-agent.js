1c348d8e413079db29d48992474f566e50f76c63
ChanKun <chan.nattawat@gmail.com>
2018-12-18 14:59:07 +0700

    test

--- src/v2-agent.js
+++ src/v2-agent.js
@@ -265,8 +265,74 @@ class V2Agent {
         responseJson.triggerEndOfConversation = this.agent.endConversation_;
       }
       debug('Response to Dialogflow: ' + JSON.stringify(responseJson));
-      console.log(JSON.stringify(responseJson));
-      this.agent.response_.json(responseJson);
+      //console.log(JSON.stringify(responseJson));
+      //this.agent.response_.json(responseJson);
+      var testJson = {"fulfillmentMessages":[  
+        {  
+           "text":{  
+              "text":[  
+                 "ทดสอบจ้า"
+              ]
+           },
+           "platform":"FACEBOOK"
+        },
+        {  
+           "text":{  
+              "text":[  
+                 "TestTestTest"
+              ]
+           },
+           "platform":"FACEBOOK"
+        },
+        {  
+           "payload":{  
+              "facebook":{  
+                 "attachment":{  
+                    "type":"template",
+                    "payload":{  
+                       "template_type":"generic",
+                       "elements":[  
+                          {  
+                             "title":"Welcome!",
+                             "image_url":"https://petersfancybrownhats.com/company_image.png",
+                             "subtitle":"We have the right hat for everyone.",
+                             "default_action":{  
+                                "type":"web_url",
+                                "url":"https://petersfancybrownhats.com/view?item=103",
+                                "webview_height_ratio":"tall"
+                             },
+                             "buttons":[  
+                                {  
+                                   "type":"web_url",
+                                   "url":"https://petersfancybrownhats.com",
+                                   "title":"View Website"
+                                },
+                                {  
+                                   "type":"postback",
+                                   "title":"Start Chatting",
+                                   "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                                }
+                             ]
+                          }
+                       ]
+                    }
+                 }
+              }
+           },
+           "platform":"FACEBOOK"
+        },
+        {  
+           "text":{  
+              "text":[  
+                 "ทดสอบจ้า"
+              ]
+           }
+        }
+     ]};
+
+
+      //this.agent.response_.json(responseJson);
+      this.agent.response_.json(testJson);
     }
   }
 
